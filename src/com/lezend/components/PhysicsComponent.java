4bd0fe77c8c111c37f754c4cab7dbc6253d590a4
drewmalin <drewmalin@gmail.com>
2012-12-22 01:47:22 -0800

    Committing a bunch of blank classes

---
+++ src/com/lezend/components/PhysicsComponent.java
@@ -0,0 +1,11 @@
+package com.lezend.components;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: drewmalin
+ * Date: 12/22/12
+ * Time: 1:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PhysicsComponent {
+}
---
+++ src/com/lezend/managers/GraphicsManager.java
@@ -0,0 +1,12 @@
+package com.lezend.managers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: drewmalin
+ * Date: 12/22/12
+ * Time: 1:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum GraphicsManager {
+
+}
